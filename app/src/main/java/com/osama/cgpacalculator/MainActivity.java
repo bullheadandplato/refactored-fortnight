bd21a46968558f1130fd90f8e5eb0a1a87ed8f41
bullhead <m.osamabinomar@gmail.com>
2017-07-09 22:32:07 +0500

    few chanegs

--- app/src/main/java/com/osama/cgpacalculator/MainActivity.java
+++ app/src/main/java/com/osama/cgpacalculator/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -30,6 +31,9 @@ public class MainActivity extends AppCompatActivity implements SubjectsAdapter.S
 
         setupRecyclerView();
         animateWhat();
+
+        //Toolbar toolbar= (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
         findViewById(R.id.calculate_button).setOnClickListener(new View.OnClickListener() {
             @Override
